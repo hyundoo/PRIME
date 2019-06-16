a88586bf638c9c4a323b9fd46536616ebda9996d
hyundoo <hdoo.jeong@gmail.com>
2019-06-16 22:15:08 +0900

    Update RcppExports.R

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -2,5 +2,5 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 prime_core <- function(x, C, sc_data, min_nbr, max_nbr) {
-    .Call(`_PRIME_prime_core`, x, C, sc_data, min_nbr, max_nbr)
+    .Call(`_PRIME_prime_core`, PACKAGE = 'PRIME', x, C, sc_data, min_nbr, max_nbr)
 }
